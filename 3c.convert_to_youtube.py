ea49890946dc6c9076a4d23f6bcda88387f66548
RaghavAggarwal0310 <raghavaggarwal03.ra@gmail.com>
2021-05-20 21:16:29 +0530

    gitignore and improved 3c.convert_to_youtube

--- 3c.convert_to_youtube.py
+++ 3c.convert_to_youtube.py
@@ -1,22 +1,23 @@
 import os
-path = os.getcwd() + '\\files'
 
-file = open(path + '\\transcript_yt.txt','r')
-count = 0
-L=[]
+path = os.getcwd() + "\\files"
+file = open(path + "\\transcript_yt.txt", "r")
+
+cnt, flag = 0, 0
+content = []
+
 for line in file:
-    L.append(line.strip())
-    count+=1
+    content.append(line.strip())
+    if len(content[-1]) == 5 and ":" in content[-1]:
+        if flag:
+            content.insert(-1, " ")
+            cnt += 1
+            flag ^= 1
+    cnt += 1
+    flag ^= 1
 file.close()
 
-for i in range(0,count,2):
-    L[i]='('+L[i]+')'
-M=L.copy()
-final = []
-for i in range(0,count,2):
-    final.append(L[i]+' '+M[i+1])
-
-f = open(path + '\\transcript.txt', 'w')
-for i in final:
-    f.write(i)
-    f.write('\n')
+f = open(path + "\\transcript.txt", "w")
+for i in range(0, cnt, 2):
+    row = content[i] + " " + content[i + 1] + "\n"
+    f.write("({}) {}\n".format(content[i], content[i + 1]))
