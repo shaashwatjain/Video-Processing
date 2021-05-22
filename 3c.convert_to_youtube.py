e16ec3b0e59b828f47ca2ef28472e1746b0eb863
RaghavAggarwal0310 <raghavaggarwal03.ra@gmail.com>
2021-05-22 20:36:05 +0530

    Updated transcript conversion handling all cases

--- 3c.convert_to_youtube.py
+++ 3c.convert_to_youtube.py
@@ -1,23 +1,55 @@
 import os
+import sys
+import time
+
 
 path = os.getcwd() + "\\files"
 file = open(path + "\\transcript_yt.txt", "r")
+full_path = path + "\\transcript_yt.txt"
 
 cnt, flag = 0, 0
 content = []
 
+if os.stat(full_path).st_size == 0:
+    print("Already present transcript is empty...")
+
+def isTimeFormat(value):
+    try:
+        time.strptime(value, '%M:%S')
+        return True
+    except ValueError:
+        return False
+
 for line in file:
-    content.append(line.strip())
-    if len(content[-1]) == 5 and ":" in content[-1]:
+    row = line.strip()
+    if isTimeFormat(row):
         if flag:
-            content.insert(-1, " ")
-            cnt += 1
-            flag ^= 1
-    cnt += 1
-    flag ^= 1
+            content.pop(-1)
+            cnt-=1
+        else:
+            flag=1
+
+    else:
+        if not flag:
+            continue
+        flag = 0
+        if row=="" and cnt>1:
+            content.pop(-1)
+            cnt-=1
+            continue
+    content.append(row)
+    cnt+=1
+
+if flag==1 and content:
+    content.pop(-1)
+    cnt-=1
 file.close()
 
 f = open(path + "\\transcript.txt", "w")
+if cnt<2:
+    print("Transcript is not correct")
+    sys.exit()
+
 for i in range(0, cnt, 2):
     row = content[i] + " " + content[i + 1] + "\n"
     f.write("({}) {}\n".format(content[i], content[i + 1]))
