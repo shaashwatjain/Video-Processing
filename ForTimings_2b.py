526d75d7f830c12b5fdccf4ae01903d522bf7210
Shaashwat <shaashjain213@gmail.com>
2021-05-26 19:27:22 +0530

    added support of irregular spaces: transcript file

--- ForTimings_2b.py
+++ ForTimings_2b.py
@@ -1,14 +1,16 @@
 from datetime import datetime
-
+import re
 
 def timings(videoname):
     transcript_path = videoname + ".txt"
     f = open(transcript_path, "r")
     timings = [0.0]
-    text = f.readline().strip()
-    while text:
-        x = datetime.strptime(text, "%M:%S")
-        timings.append(float(x.minute * 60 * 1000 + x.second * 1000))
-        f.readline().strip()
-        text = f.readline().strip()
+    for text in f:
+        text = text.strip()
+        mat = re.match('\d{2}:\d{2}',text)
+        # print(text, mat)
+        if mat:
+            x = datetime.strptime(text, "%M:%S")
+            timings.append(float(x.minute * 60 * 1000 + x.second * 1000))
+    # print(timings)
     return timings
